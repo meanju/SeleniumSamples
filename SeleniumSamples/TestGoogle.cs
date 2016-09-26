291e99b28138e3fdd98fed7f168020f65d7bf9b5
Anju Khatri <khatri.anjali@gmail.com>
2016-09-25 23:25:49 -0400

    First Commit

---
+++ SeleniumSamples/TestGoogle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class TestGoogle
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            driver = new OpenQA.Selenium.IE.InternetExplorerDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+           
+        }
+
+        [TestMethod]
+        public void TheGooglesearchTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/?gws_rd=ssl");
+         
+
+            Assert.IsTrue(verificationErrors.Length == 0);
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
