114472afb98b9dab78eb99ff72e531228c3bf856
Anju Khatri <khatri.anjali@gmail.com>
2017-01-08 22:01:59 -0500

    added project for executing with exe console

---
+++ SeleniumSamplesWinForm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeleniumSamplesWinForm
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        
+        public static void Main(string []args)
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            WorkingWithFirefox ff = new WorkingWithFirefox();
+            ff.InvokeBrowser();
+            ff.CloseAllBrowser();
+            //Console.Read();
+        }
+    }
+}
---
+++ SeleniumSamplesWinForm/WorkingWithFirefox.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumSamplesWinForm
+{
+    public class WorkingWithFirefox
+    {
+        private IWebDriver driver = new FirefoxDriver();
+        string url = "http://www.google.com";
+
+        public void InvokeBrowser()
+        {
+            driver.Manage().Window.Maximize();
+
+            driver.Manage().Cookies.DeleteAllCookies();
+
+            driver.Url = url;
+
+            var titleOfPage = driver.Title;
+
+            Console.WriteLine("Title of the page is: " + titleOfPage);
+
+            var currentUrl = driver.Url;
+
+            Console.Error.WriteLine("Title of the page is " + currentUrl);
+
+            var pageSource = driver.PageSource;
+
+            Console.WriteLine("Page Source" + pageSource);
+
+        }
+
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+
+        public void CloseAllBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
